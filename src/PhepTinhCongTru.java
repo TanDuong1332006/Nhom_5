e1dfeed1e73ef3bab631ac5cb938e58ef3ccae53
giangmy170526 <mudynguyen@gmail.com>
2026-04-21 20:00:52 +0700

    Add files via upload
    kéo src vô thử

---
+++ src/PhepTinhCongTru.java
@@ -0,0 +1,10 @@
+public class PhepTinhCongTru {
+    
+    public static double cong(double a, double b) {
+        return a + b;
+    }
+
+    public static double tru(double a, double b) {
+        return a - b;
+    }
+}
